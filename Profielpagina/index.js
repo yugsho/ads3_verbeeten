f3a5f24e0a2a4cd7cb9ab79a55353807ced62469
Jelle Tiepel <414532@student.fontys.nl>
2020-11-15 17:31:39 +0100

    Profielpagina compleet
    De benodigde elementen zijn toegevoegd

--- Profielpagina/index.js
+++ Profielpagina/index.js
@@ -6,14 +6,20 @@ import image from './Woman.jpeg';
 class Profiel extends React.Component{
   render(){
     return(
-      <section id="headerProfiel">
-        <img src={image} alt="Profielfoto van betreffend persoon"/>
-        <h1 id="naamProfiel">Carla</h1>
-        <p id="introProfiel">Hoi ik ben Carla! ik werk in het ziekenhuis bij de oncologie afdeling. Hier zie ik elke dag mensen leiden aan kanker. Ik loop elk jaar mee met de Verbeeten Challenge
-        omdat ik wil dat er betere behandelmethodes komen tegen kanker. Ik zou het heel erg waarderen als jullie mij hier in willen steunen met een bijdrage of door met mij mee
-        te lopen en zelf nog meer geld op te halen! </p>
-        <button type="button">Steun Mij</button>
-        <button type="button">Login</button>
+      <section id="headerProfiel">  //De hele sectie voor de profielinformatie
+        <div id="imageProfiel"> //Aparte div voor ruimte tussen de foto, maakt de werking van de flex iets makkelijker
+          <img src={image} alt="Profielfoto van betreffend persoon"/>
+        </div>
+        <div id="informatieProfiel">
+          <h1 id="naamProfiel">Carla</h1>
+          <p id="introProfiel">Hoi ik ben Carla! ik werk in het ziekenhuis bij de oncologie afdeling. Hier zie ik elke dag mensen leiden aan kanker. Ik loop elk jaar mee met de Verbeeten Challenge
+          omdat ik wil dat er betere behandelmethodes komen tegen kanker. Ik zou het heel erg waarderen als jullie mij hier in willen steunen met een bijdrage of door met mij mee
+          te lopen en zelf nog meer geld op te halen! </p>
+          <div id="buttons"> //Aparte div voor de buttons zodat deze een eigen flex hebben
+            <button type="button">Steun Mij</button>
+            <button type="button">Login</button>
+          </div>
+        </div>
       </section>
     );
   }
@@ -21,9 +27,9 @@ class Profiel extends React.Component{
 class Donaties extends React.Component{
   render(){
     return(
-      <main>
+      <main>  //Het hele gedeelte van de donaties
         <h2 id="kopDonaties">Donaties</h2>
-        <div className="donatieAan">
+        <div className="donatieAan">  //Iedere div is een aparte donatie
           <section className="dataDonatie">
             <h2 className="naamPersoon">Anoniem</h2>
             <p className="datumDonatie">28 0ktober 2020</p>
